35dfc2956520543eb6b6345ebb2afd1e773b88dc
Essam Almuqri <essam.almuqri@gmail.com>
2018-12-09 21:37:37 +0300

    Added static content, json message validation & Webserver along side websocket

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 'use strict';
+const fs = require('fs');
 const WebSocket = require('ws');
+const http = require('http');
+const port = 8080;
 
 // const user = require('./models/user');
 // const message = require('./models/message');
 // const group = require('./models/group');
 
+// const wss = new WebSocket.Server({
+//     port: port
+// });
+const server = http.createServer(function (request, response) {
+    const html = fs.readFileSync('./public/index.html');
+    response.write(html);
+    response.end();
+}).listen(port);
 const wss = new WebSocket.Server({
-    port: 8080
+    server: server
 });
+
+
 const clientsList = {};
 wss.on('connection', function connection(ws) {
 
@@ -16,7 +29,12 @@ wss.on('connection', function connection(ws) {
 
 
     ws.on('message', function incoming(message) {
-        parseMessage(message, ws);
+        try {
+            message = JSON.parse(message);
+            parseMessage(message, ws);
+        } catch (error) {
+            console.log('not JSON message passed');
+        }
         // const messageContent = JSON.parse(message);
         // sendToClient(messageContent.to);
     });
@@ -43,7 +61,7 @@ function sendToClient(userId) {
 
 // parse message to determine what kind of message and best action.
 function parseMessage(message, ws) {
-    message = JSON.parse(message);
+
 
     switch (message.type) {
         case "directMessage":
@@ -70,11 +88,11 @@ function sendDirectMessage(message) {
     const ws = clientsList[message.destination];
     console.log(message);
     console.log(message.destination);
-    
+
     if (ws) {
         ws.send(message.content);
-    }else{
-        console.log('message not sent');   
+    } else {
+        console.log('message not sent');
     }
 }
 
@@ -82,6 +100,6 @@ function registerClient(message, ws) {
     const user = message.user;
     clientsList[user.email] = ws;
     console.log('Client ID: ' + user.email + " connected!");
-    
+
     // return user;
 }
