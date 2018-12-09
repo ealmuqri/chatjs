7635d211bb32fc7e95f52593c25372e2884541b1
Essam Almuqri <essam.almuqri@gmail.com>
2018-12-09 07:44:37 +0300

    [major]- made app structure and decided message types keys

--- index.js
+++ index.js
@@ -1,41 +1,82 @@
+'use strict';
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+// const user = require('./models/user');
+// const message = require('./models/message');
+// const group = require('./models/group');
+
+const wss = new WebSocket.Server({
+    port: 8080
+});
 const clientsList = {};
 wss.on('connection', function connection(ws) {
 
-    const userId = creatClientId(ws);
+    // const user = creatClientId(ws);
+    // console.log(user);
+
 
     ws.on('message', function incoming(message) {
-        console.log('recieved from: ' + userId);
-        console.log('received: %s', message);
-        // parseMessage(message);
-        const messageContent = JSON.parse(message);
-        sendToClient(messageContent.to);
+        parseMessage(message, ws);
+        // const messageContent = JSON.parse(message);
+        // sendToClient(messageContent.to);
     });
-    ws.send('Welcome ' + userId);
+
+    // ws.send('Welcome ' + user);
 });
 
+
 function creatClientId(ws) {
-    const userId = Math.random();
-    clientsList[userId] = ws;
-    console.log('Client ID: ' + userId + " connected!");
-    return userId;
+    // const userId = Math.random();
+    // const userObj = user;
+    user.id = Math.random();
+    user.email = 'ealmuqri';
+    clientsList[user.email] = ws;
+    console.log('Client ID: ' + user.email + " connected!");
+    return user;
 }
 
 
 // takes userId input and searches ClientsList then, send message to User
 // 0.8864452392915056
 function sendToClient(userId) {
-    const ws = clientsList[userId];
+
+}
+
+// parse message to determine what kind of message and best action.
+function parseMessage(message, ws) {
+    message = JSON.parse(message);
+
+    switch (message.type) {
+        case "directMessage":
+            sendDirectMessage(message);
+            break;
+        case "registerClient":
+            registerClient(message, ws);
+            break;
+        default:
+            break;
+    }
+    // if (message.type === 'directMessage') {
+    //     handleDirectMessage(message);
+    // } else {
+    //     if (message.type === 'registerClient') {
+
+    //     }
+    // }
+}
+
+function sendDirectMessage(message) {
+    // TODO: validate message.to
+    // TODO: construct message structure.
+    const ws = clientsList[message.destination];
     if (ws) {
-        console.log('Message sent to: ' + userId);
-        ws.send('you got new message');
+        ws.send(message.content);
     }
 }
 
-// parse message
-function parseMessage(message) {
-    const data = JSON.parse(message);
-    console.log('Message to: ' + data.to);
+function registerClient(message, ws) {
+    const user = message.user;
+    clientsList[user.email] = ws;
+    console.log('Client ID: ' + user.email + " connected!");
+    // return user;
 }
