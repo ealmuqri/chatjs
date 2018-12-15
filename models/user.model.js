21bcbdad1b118bc7e57925063d7ab22fb1fd067b
Essam Almuqri <essam.almuqri@gmail.com>
2018-12-15 19:32:27 +0300

    Store message in MongoDB

--- models/user.model.js
+++ models/user.model.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 
 class User {
     constructor(user) {
-        // this.id = user.id;
-        // this.name = user.name;
-        // this.email = user.email;
-        // this.bioMessage = user.bioMessage;
-        // this.status = user.status;
-        // this.friends = user.friends;
-        // this.blockedUsers = user.blockedUsers;
-        // this.starredMessages = user.starredMessages;
-        // this.archivedChats = user.archivedChats;
+        this.id = user.id;
+        this.name = user.name;
+        this.email = user.email;
+        this.bioMessage = user.bioMessage;
+        this.status = user.status;
+        this.friends = user.friends;
+        this.blockedUsers = user.blockedUsers;
+        this.starredMessages = user.starredMessages;
+        this.archivedChats = user.archivedChats;
 
         const userModel = this.getMongoModel(user);
 
